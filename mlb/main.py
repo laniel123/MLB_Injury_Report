0812033f00c867e97cef8c649bd776871da47cc7
Daniel L <lanieldarson@gmail.com>
2025-05-24 16:06:58 -0700

    File formating and notebook changes

---
+++ mlb/main.py
@@ -0,0 +1,11 @@
+"""
+This is the main pipeline that will: 
+
+ 1. scrape data for known players that have been injured in the past. 
+ 2. train a model to predict injury risk based on player stats.
+ 3. Predict injury risk for current players based on their stats.
+ 4. (Opiional) Save data to csv files for future use.
+ 
+    
+"""
+
